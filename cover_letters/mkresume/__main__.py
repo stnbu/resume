b1543fae9c030d397f3650a31ce9d40beea097e2
Mike Burr <mburr@unintuitive.org>
2018-10-11 16:50:14 -0700

    m

---
+++ cover_letters/mkresume/__main__.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+
+import sys
+import re
+import argparse
+from _parts import *
+
+ARGS = None
+SIGNATURE = """Mike Burr
+mburr@unintuitive.org
+https://unintuitive.org/
+"""
+THANKS = 'Thank you.'
+
+URL_MARKDOWN_RE = r'\<\<(?P<text>.*?)\|(?P<url>.*?)\>\>'
+
+class Exit(Exception):
+    None
+    
+def mkurl(m):
+    d = m.groupdict()
+    if ARGS.format == 'text':
+        return '%s [%s]' % (d['text'], d['url'])
+    elif ARGS.format == 'html':
+        return '<a href="%s">%s</a>' % (d['url'], d['text'])
+    else:
+        raise Exit('Unknown format: %s' % fmt)
+
+def get_resume():
+
+    lines = []
+    lines.append(ARGS.salutation)
+    lines.append('')
+    for name in ARGS.paragraphs:
+        try:
+            line = globals()['X_%s' % name.upper()]
+        except NameError:
+            raise Exit('Unknown paragraph name: %s' % name)
+        if name.upper() == 'PLEASE_CONSIDER':
+            line = line.format(position='<<%s|%s>>' % (ARGS.job_title, ARGS.job_url))
+        lines.append(line)
+        lines.append('')
+    lines.append(THANKS)
+    lines.append('')
+    lines.append(SIGNATURE)
+
+    def foo(string):
+        return re.sub(URL_MARKDOWN_RE, mkurl, string)
+
+    lines = map(foo, lines)
+    
+    return '\n'.join(lines)
+
+def get_args(argv):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--salutation', choices=['Hello!', 'Greetings!'], default='Hello!')
+    parser.add_argument('--format', choices=['text', 'html'], default='html')
+    parser.add_argument('--job-title', required=True)
+    parser.add_argument('--job-url', required=True)
+    parser.add_argument('--list', default=False, action='store_true')
+    parser.add_argument('paragraphs', nargs='*')
+    return parser.parse_args(argv)
+
+def print_paragraph_choices():
+    for name, _ in globals().items():
+        if name.startswith('X_'):
+            print(name[2:])
+
+if __name__ == '__main__':
+
+    ARGS = get_args(sys.argv)
+    ARGS.paragraphs = ARGS.paragraphs[1:]
+    if ARGS.list:
+        print_paragraph_choices()
+        sys.exit(0)
+    for name in vars(ARGS):
+        print(name, getattr(ARGS, name))
+    print(get_resume())
---
+++ cover_letters/mkresume/_parts.py
@@ -0,0 +1,29 @@
+# -*- mode: python; coding: utf-8 -*-
+
+X_PLEASE_CONSIDER = """Please consider my application for the possition of {position}. I have attached my resume in MS Word format, but also invite you to view my <<online resume|https://unintuitive.org/static/main/bin/resume-mike-burr.html>> which offers a bit more detail and interactivity."""
+
+X_STARTUP = """The first half of my career I spent with small, efficient, start-up-sized organizations. I am familiar with the pace of work and expectations for quality and commitment that come with such an environment. Not only am I familiar and comfortable with such an environment, I’ve found that I prefer it! Meaningful work is the key to a happy work life."""
+
+X_WFH = """I also have a total of nearly three years of productive work-from-home experience, working for Cisco from my home office in Japan. I was able to effectively communicate with my peers and remain very productive despise the distance and time zone difference. Cisco was pleased enough with my work to offer me a promotion and pay for my relocation back to the US, which I accepted. I continued to work for Cisco for another two years."""
+
+X_PYTHON = """I have been a Python user for 10+ years and have used Python daily as a professional programmer for about six years, most recently in my position as a staff engineer at VMware. This role required automation of a range of interactions with different cloud providers and technologies. Much of their tooling and I suspect many of their challenges are similar to those of Monetate's development operations. I also have a passion for quality and efficiency. Time spent "doing it right" is never time misspent."""
+
+X_DJANGO = """I have used Django for numerous projects throughout my career and have recently been brushing up on and polishing my skills by hosting and maintaining some personal web sites:
+
+    • http://hijirishimamoto.com/ (source: https://github.com/stnbu/sws)
+    • https://unintuitive.org/ (source: https://github.com/stnbu/oleo)
+
+I keep busy with plenty of other interesting personal projects. Please feel free to browse <<my GitHub account|https://github.com/stnbu>>."""
+
+X_JAVASCRIPT = """I have used JavaScript off and on throughout my career. It's not as strong as my python, but the more I learn, the more interested I become. I've made some inroads on understanding React and will be updating my personal projects to use React soon. Totally comprehensible."""
+
+X_WEB_BASED_UTILS = """My last two jobs involved work on maintaining and extending very complex testing frameworks, both of which included web UI functionality. Additionally, I've written some utilities DevOps-related utilities with a WebUI. They're no longer maintained, but are worth having a look at: 
+
+https://github.com/stnbu/dunaway
+https://github.com/stnbu/centrog
+
+(The above happen to be written using Flask.)"""
+
+X_LINUX = """Up until a few years ago, my every-day machine had been a linux desktop (first Debian, then Ubuntu, with a smattering of other distros. I now use a Mac.) No problems getting around and troubleshooting. I am very much at home. I still use linux daily "on the server side" and have been for years."""
+
+X_GOOD_CODING = """I like readable, efficient, reusable code. I like to think I've learned to strike the right balance. PEP8 is a reasonable starting point. I am a strong believer in all kinds of static code analysis, so pyflakes, etc has been in my toolbox for years."""
